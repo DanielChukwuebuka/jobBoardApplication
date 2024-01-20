04794c0f6eee0d7ad82d88f6d7d9410ea97cbcd6
jerrycho3 <vazzle333@gmail.com>
2024-01-20 09:45:53 -0800

    newPush

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 
-const express = require('express');
+const express = require('express')
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+origin: '*',
+methods: ["GET"]
+}))
 
 require("./config/config")
 
